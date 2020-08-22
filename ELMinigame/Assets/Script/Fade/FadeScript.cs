a73499b5341a1ead257e2b052f634c4eff6ff54f
hyuga0305 <csofumry@gmail.com>
2020-08-22 16:02:58 +0900

    タイトルシーンのUIを仮置き

---
+++ ELMinigame/Assets/Script/Fade/FadeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    private RectTransform rc;
+    [SerializeField] private float speed = 0.01f;
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+    public bool EndFade = false;
+
+    void Start() {
+        rc = GetComponent<RectTransform>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update() {
+        if (fadeIn || fadeOut) {
+            if (fadeIn) {
+                rc.sizeDelta = new Vector2(rc.sizeDelta.x + speed * 16.0f, rc.sizeDelta.y + speed * 9.0f);
+                if(rc.sizeDelta.x >= 1600.0f) {
+                    fadeIn = false;
+                    EndFade = true;
+                }
+            }
+            else {
+                rc.sizeDelta = new Vector2(rc.sizeDelta.x - speed * 16.0f, rc.sizeDelta.y - speed * 9.0f);
+                if (rc.sizeDelta.x <= 0.0f) {
+                    fadeOut = false;
+                    EndFade = true;
+                }
+            }
+        }
+    }
+
+    public void FadeIn() {
+        fadeIn = true;
+        EndFade = false;
+    }
+
+    public void FadeOut() {
+        fadeOut = true;
+        EndFade = false;
+    }
+}
