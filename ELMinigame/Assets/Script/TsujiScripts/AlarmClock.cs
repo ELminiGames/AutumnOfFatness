d08d646519c0c94dd63ea9df6393397ce84f2be9
ktsuji <aoriochiki@gmail.com>
2020-09-12 01:15:37 +0900

    スクリプトを修正

--- ELMinigame/Assets/Script/TsujiScripts/AlarmClock.cs
+++ ELMinigame/Assets/Script/TsujiScripts/AlarmClock.cs
@@ -6,7 +6,6 @@ public class AlarmClock : MonoBehaviour
 {
     [SerializeField] private float interval;        //振動している時間
     [SerializeField] private float RandomValue;
-    [SerializeField] private GameObject clockObj;
 
     [SerializeField] private float deltaTime;
     private bool  isBell;
@@ -51,15 +50,15 @@ public class AlarmClock : MonoBehaviour
 
     private void InitAlarmClock()
     {
-        clockObj.GetComponent<Transform>().localRotation = Quaternion.identity;
+        GetComponent<Transform>().localRotation = Quaternion.identity;
     }
 
     private void VibreateAlarmClock()
     {
         float val1 = Random.Range(-RandomValue, RandomValue);
-        Quaternion quaternion = clockObj.GetComponent<Transform>().localRotation;
+        Quaternion quaternion = GetComponent<Transform>().localRotation;
         quaternion.z = val1;
-        clockObj.GetComponent<Transform>().localRotation = quaternion;
+        GetComponent<Transform>().localRotation = quaternion;
     }
 
     public bool GetBellFlag()
