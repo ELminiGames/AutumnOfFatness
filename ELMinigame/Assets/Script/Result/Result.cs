92f5c15aeef5ec33332fbfde39c70cd24b923630
hyuga0305 <csofumry@gmail.com>
2020-09-12 12:42:10 +0900

    リザルトプレハブ・スクリプトを作成

---
+++ ELMinigame/Assets/Script/Result/Result.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    [SerializeField] private AudioClip SE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void ShowResult(string EndTime) {
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        GameObject ResultTime = gameObject.transform.GetChild(1).gameObject;
+        ResultTime.GetComponent<Text>().text = EndTime;
+        gameObject.GetComponent<AudioSource>().PlayOneShot(SE);
+    }
+}
