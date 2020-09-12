9be5b24779bf5eac09c6298d4637c44e66e62775
hyuga0305 <csofumry@gmail.com>
2020-09-12 10:47:47 +0900

    シリアル通信とレイショットスクリプトを追加

--- ELMinigame/Assets/Script/Cat/MoveCat.cs
+++ ELMinigame/Assets/Script/Cat/MoveCat.cs
@@ -7,9 +7,11 @@ public class MoveCat : MonoBehaviour
     private Rigidbody rb;
     private Animator animator;
     private GameObject Player;
+    [SerializeField] private WakingGauge wakingGauge;
     [SerializeField] private float MoveSpeed;
     [SerializeField] private float WaitMaxTime;
     [SerializeField, Range(0, 100)] private float WarningPer;
+    [SerializeField] private float WarkingAdd;
     private bool Stop = false;
     private float StopTime = 0.0f;
 
@@ -56,6 +58,12 @@ public class MoveCat : MonoBehaviour
         }
     }
 
+    public void Escape() {
+        float NewAngle = 180.0f * Random.value;
+        gameObject.transform.eulerAngles = new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y + NewAngle, gameObject.transform.eulerAngles.z);
+        Stop = false;
+    }
+
     private void OnCollisionEnter(Collision collision) {
         if(collision.gameObject.tag == "Wall") {
             animator.SetBool("Walk", false);
@@ -75,7 +83,7 @@ public class MoveCat : MonoBehaviour
     private void OnTriggerStay(Collider other) {
         if (other.tag == "Player") {
             StopTime += Time.deltaTime;
-            // ヘイト追加処理
+            wakingGauge.SetGauge(WarkingAdd);
         }
     }
 }
---
+++ ELMinigame/Assets/Script/RayShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShot : MonoBehaviour
+{
+    [SerializeField] private GameObject hit_target;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit_info = new RaycastHit();
+            float max_distance = 1000f;
+
+            bool is_hit = Physics.Raycast(ray, out hit_info, max_distance);
+
+            if (is_hit) {
+                if (hit_info.transform.name == hit_target.name) {
+                    hit_target.GetComponent<MoveCat>().Escape();
+                }
+            }
+        }
+    }
+}
